7af2ad31ea3a478e0f7e71fe0394f06fdcdc7d05
dhurley94 <drhurley94@gmail.com>
2017-11-02 05:35:03 -0400

    add unused pkgs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const burgers = require('./controllers/burgers_controller');
 const path = require('path');
