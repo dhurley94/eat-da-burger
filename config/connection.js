3d8b27e682429c4e107aa16212695d4825d2fe21
dhurley94 <drhurley94@gmail.com>
2017-11-02 00:22:43 -0400

    jawsdb integ

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: 'cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'mw48a0sjz64bevq3',
     database: 'burgers_db',
+    password: 'fxhh2r5xenrr15cy',
 });
 
 module.exports = connection;
