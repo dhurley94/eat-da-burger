6209558c33fab9d2eb9c774976af79d6c30db0f3
dhurley94 <drhurley94@gmail.com>
2017-11-01 22:43:42 -0400

    schema syntax & conn string

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,9 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const Connection = mysql.createConnection({
-    host: 'dhurley.tk',
-    user: 'rizse_burgers',
-    database: 'rizse_burgers',
-    password: "I=#T_JH.ZmCH"
+    host: 'localhost',
+    user: 'root',
+    database: 'burgers_db',
 });
 
 module.exports = Connection;
